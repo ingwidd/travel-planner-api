9348001fe7fd1a805a47f7f0bee27e43747862b4
Ingrid Seah <ingridseah@gmail.com>
2026-07-24 10:33:59 +0800

    new commit

--- index.js
+++ index.js
@@ -567,4 +567,10 @@ app.get('/', (req, res) => {
   console.log('App is listenig on port 3000');
 });*/
 
+if (process.env.NODE_ENV !== 'production') {
+  app.listen(3000, () => {
+    console.log('App is listening on port 3000');
+  });
+}
+
 module.exports = app;
